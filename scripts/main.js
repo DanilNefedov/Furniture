7a5962eb7eeeff48d481bf5dfe9f3c4c465f91ee
DanilNefedov <ndanil2@ukr.net>
2022-06-16 18:24:17 +0300

    main swiper js\arrow carousel\without adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 const menu = document.getElementById('menu');
 const nav = document.getElementById('nav');
-const widthWind =  window.screen.width;
+const widthWind = window.screen.width;
 
 
 menu.addEventListener('click', open);
 
-function open(e){
+function open(e) {
 	e.preventDefault();
 	menu.classList.toggle('active');
 
@@ -14,16 +14,52 @@ function open(e){
 	document.body.classList.toggle('scroll');
 }
 
-window.addEventListener('resize',function(e){
+window.addEventListener('resize', function (e) {
 
-	if((innerWidth >= 1000) && (nav.classList.contains('act-nav'))){
+	if ((innerWidth >= 1000) && (nav.classList.contains('act-nav'))) {
 		nav.classList.remove('act-nav');
 		menu.classList.remove('active');
 		document.body.classList.remove('scroll');
-	}else{
+	} else {
 		return false
 	}
 });
 
 
 
+
+
+let slider = document.getElementById('slider');
+let slide = document.getElementById('cont-slide');
+let widthImg = document.getElementById('width-img');
+let count = 0;
+
+
+slider.onclick = function (event) {
+	let target = event.target;
+	let widthElem = widthImg.offsetWidth;
+
+	console.log(widthElem)
+	if (target.classList.contains('arrow') || (target.tagName = 'SVG') || (target.tagName = 'PATH')) {
+
+
+		if (target.classList.contains('left') && count > widthElem * (-5)) {
+			count -= widthElem;
+
+			slide.style.cssText = `transform: translate3d(${count}px, 0px, 0px);
+		transition-duration: 1195ms;`;
+
+
+
+		} else if (target.classList.contains('right') && count < widthElem * 2) {
+			count += widthElem;
+			slide.style.cssText = `transform: translate3d(${count}px, 0px, 0px);
+		transition-duration: 1195ms;`;
+
+
+		} else {
+
+			return
+		}
+	}
+}
