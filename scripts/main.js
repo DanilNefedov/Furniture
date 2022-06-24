831cb57fd27519cb1d2b3351edb4b417d2ceb1e6
DanilNefedov <ndanil2@ukr.net>
2022-06-24 17:38:58 +0300

    swiper rooms

--- scripts/main.js
+++ scripts/main.js
@@ -37,14 +37,16 @@ let position = 0;
 
 
 slider.onclick = function (event) {
+	
+
 	let target = event.target;
 	let widthElem = widthImg.offsetWidth;
 	let num = target.dataset.slide;
-	let sliderContent = event.path[2].childNodes[3].children;
+
+	//let sliderContent = event.path[2].childNodes[3].children;
 
 	
 	
-	//console.log(sliderContent)
 
 	if (target.classList.contains('main-swiper__dot')) {
 
@@ -96,10 +98,10 @@ slider.onclick = function (event) {
 
 			
 			for(let i = 0; i < dot.length; i++){
-				console.log(dot[i]);
+				// console.log(dot[i]);
 				
 				if(dot[i].classList.contains('active-pag')){
-					console.log('sss')
+					//console.log('sss')
 					dot[i].classList.remove('active-pag');
 					dot[i+1].classList.add('active-pag');
 					break;
@@ -117,3 +119,90 @@ slider.onclick = function (event) {
 }
 
 
+const sliderRoom = document.getElementById('slider-room');
+const slideRoom = document.getElementsByClassName('rooms-swiper__elem');
+const dotRoom = document.getElementsByClassName('main-swiper__dot');
+const roomSwiperImg = document.getElementById('room-swiper-img');
+let positionRoom = 0;
+
+sliderRoom.onclick = function (event){
+	let target = event.target;
+	let num = target.dataset.room;
+	let widthElem = roomSwiperImg.offsetWidth * 1.2;
+	
+	//console.log(widthElem)
+	if (target.classList.contains('main-swiper__dot')) {
+
+		for(let i = 0; i < dotRoom.length; i++){
+			dotRoom[i].classList.remove('active-pag');
+		}
+		target.classList.add('active-pag');
+		
+
+
+		if (num > 0) {
+			positionRoom = widthElem * (-num);
+			console.log(positionRoom)
+			for(let i = 0; i < slideRoom.length; i++){
+				slideRoom[i].style.cssText = `transform: translate3d(${positionRoom}px, 0px, 0px);
+				transition-duration: 1195ms;`;
+			}
+			
+		}else if(num === '0'){
+			positionRoom = 0;
+			for(let i = 0; i < slideRoom.length; i++){
+				slideRoom[i].style.cssText = `transform: translate3d(${positionRoom}px, 0px, 0px);
+				transition-duration: 1195ms;`;
+			}
+		}	
+	}
+
+
+
+
+	if (target.classList.contains('arrow') || (target.tagName = 'SVG') || (target.tagName = 'PATH')) {
+
+		console.log(positionRoom)
+		if (target.classList.contains('left') && positionRoom > 0) {
+			positionRoom += widthElem;
+
+			// for(let i = 0; i < dot.length; i++){
+			// 	if(dot[i].classList.contains('active-pag')){
+			// 		dot[i-1].classList.add('active-pag')
+			// 	}
+			// 	dot[i].classList.remove('active-pag');
+			// }
+
+			for(let i = 0; i < slideRoom.length; i++){
+				slideRoom[i].style.cssText = `transform: translate3d(${positionRoom}px, 0px, 0px);
+				transition-duration: 1195ms;`;
+			}
+			
+
+
+		} else if (target.classList.contains('right') && (positionRoom > widthElem * -3)) {
+			console.log(positionRoom);
+			console.log(widthElem * 3)
+			positionRoom -= widthElem;
+
+			for(let i = 0; i < slideRoom.length; i++){
+				slideRoom[i].style.cssText = `transform: translate3d(${positionRoom}px, 0px, 0px);
+				transition-duration: 1195ms;`;
+			}
+			
+			// for(let i = 0; i < dot.length; i++){
+			// 	// console.log(dot[i]);
+				
+			// 	if(dot[i].classList.contains('active-pag')){
+			// 		//console.log('sss')
+			// 		dot[i].classList.remove('active-pag');
+			// 		dot[i+1].classList.add('active-pag');
+			// 		break;
+			// 	}
+			// }
+
+		} else {
+			return
+		}
+	}
+}
