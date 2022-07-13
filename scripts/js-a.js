f6c2fa4582c7da0dd5c52e683cbc498990e5b7b2
DanilNefedov <ndanil2@ukr.net>
2022-07-13 19:57:37 +0300

    json main swiper about without adaptive

--- scripts/js-a.js
+++ scripts/js-a.js
@@ -2,24 +2,25 @@ import jsonConfig from "./data-main-swiper-about.json" assert {type:"json"};
 console.log(jsonConfig)
 
 const mainSwiperAbout = document.querySelectorAll('.main-swiper__about');
-const addBlockMainSwiper = document.getElementById('main-hover-about')
+const addBlockMainSwiper = document.getElementById('main-hover-about');
+const mainHoverContent = document.getElementById('main-hover-content');
+
 
 
 for(let i = 0; i < mainSwiperAbout.length; i++){
     mainSwiperAbout[i].addEventListener('click', ()=>{
-        console.log(i);
-        console.log(jsonConfig[i]);
-
-        document.body.classList.add('act-body')
         
-        addBlockMainSwiper.insertAdjacentHTML('afterbegin', getCoord(jsonConfig, i))
-        addBlockMainSwiper.style.top = '0';
-        addBlockMainSwiper.style.right = '0';
+        mainHoverContent.insertAdjacentHTML('afterbegin', getCoord(jsonConfig, i))
+        addBlockMainSwiper.style.top = '0px';
+        addBlockMainSwiper.style.right = '0px';
         
     })
 }
 
 function getCoord(obj, index){
+    while (mainHoverContent.firstChild) {
+        mainHoverContent.removeChild(mainHoverContent.firstChild);
+    }
     return(
         `<div class="main-swiper__header">
             ${obj[index].header}
@@ -27,18 +28,35 @@ function getCoord(obj, index){
 
         <div class="main-swiper__img">
             <img src="${obj[index].img}" alt="furnitura">
+            
         </div>
-        <div class="main-swiper__img-price">
-            ${(obj[index].price || "")}
-        </div>
+        <div class="main-swiper__price">
+            <div class="main-swiper__price-new">
+                ${(obj[index].price || "")}
+            </div>
 
-        <div class="main-swiper__img-discount">
-            ${(obj[index].discount || "")}
+            <div class="main-swiper__price-old">
+                ${(obj[index].oldPrice || "")}
+            </div>
         </div>
+        
+
         <div class="main-swiper__subtitle">
             ${obj[index].subtitle}
 		</div>
+        <div class="main-swiper__btn">
+            <button class="main-swiper__btn-buy">Buy</button>
+		    <button class="main-swiper__btn-add">Add to cart</button>
+        </div>
+        
         `
     );
 }
 
+addBlockMainSwiper.addEventListener('click', (e)=>{
+    if(e.target.classList.contains("bg") || e.target.classList.contains("x-mark")){
+        addBlockMainSwiper.style.top = '-100%';
+        addBlockMainSwiper.style.right = '-100%';
+    }
+    
+})
